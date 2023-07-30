490254cffbe0c90e5be624a61a24c1eda513927b
Mohammed Salem <mohammed.salem701@gmail.com>
2023-07-31 00:54:03 +0300

    fix error

--- src/scenes/DotGroup.jsx
+++ src/scenes/DotGroup.jsx
@@ -6,31 +6,31 @@ function DotGroup({selectedPage, setSelectedPage}) {
         <div className="fixed right-7 top-[50%] flex flex-col gap-6">
             <AnchorLink 
                 href="#home"
-                className={`w-3 h-3 rounded-full ${selectedPage == 'home' ? selectedStyle : 'bg-dark-grey'}`}
+                className={`w-3 h-3 rounded-full ${selectedPage === 'home' ? selectedStyle : 'bg-dark-grey'}`}
                 onClick={() => setSelectedPage('home')}
             />
 
             <AnchorLink 
                 href="#skills"
-                className={`w-3 h-3 rounded-full ${selectedPage == 'skills' ? selectedStyle : 'bg-dark-grey'}`}
+                className={`w-3 h-3 rounded-full ${selectedPage === 'skills' ? selectedStyle : 'bg-dark-grey'}`}
                 onClick={() => setSelectedPage('skills')}
             />
 
             <AnchorLink 
                 href="#projects"
-                className={`w-3 h-3 rounded-full ${selectedPage == 'projects' ? selectedStyle : 'bg-dark-grey'}`}
+                className={`w-3 h-3 rounded-full ${selectedPage === 'projects' ? selectedStyle : 'bg-dark-grey'}`}
                 onClick={() => setSelectedPage('projects')}
             />
 
             <AnchorLink 
                 href="#testimonials"
-                className={`w-3 h-3 rounded-full ${selectedPage == 'testimonials' ? selectedStyle : 'bg-dark-grey'}`}
+                className={`w-3 h-3 rounded-full ${selectedPage === 'testimonials' ? selectedStyle : 'bg-dark-grey'}`}
                 onClick={() => setSelectedPage('testimonials')}
             />
 
             <AnchorLink 
                 href="#contact"
-                className={`w-3 h-3 rounded-full ${selectedPage == 'contact' ? selectedStyle : 'bg-dark-grey'}`}
+                className={`w-3 h-3 rounded-full ${selectedPage === 'contact' ? selectedStyle : 'bg-dark-grey'}`}
                 onClick={() => setSelectedPage('contact')}
             />
         </div>
